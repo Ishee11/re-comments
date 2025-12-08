dd39ce8b10c5ca365ad1bf111b577365e8f89a64
Stepan Semenov <ishee11@gmail.com>
2025-12-08 16:11:25 +1000

    Add Comment entity

---
+++ internal/entity/comment.go
@@ -0,0 +1,48 @@
+package entity
+
+import (
+	"errors"
+	"strings"
+	"time"
+)
+
+type Comment struct {
+	ID        int64
+	EntityID  int64
+	UserID    int64
+	Text      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+var ErrEmptyCommentText = errors.New("text is empty")
+var ErrNotAuthor = errors.New("not author")
+
+func NewComment(entityID int64, userID int64, text string) (*Comment, error) {
+	text = strings.TrimSpace(text)
+	if text == "" {
+		return nil, ErrEmptyCommentText
+	}
+
+	now := time.Now().UTC()
+
+	return &Comment{
+		EntityID:  entityID,
+		UserID:    userID,
+		Text:      text,
+		CreatedAt: now,
+	}, nil
+}
+
+func (c *Comment) UpdateComment(userID int64, text string) error {
+	if userID != c.UserID {
+		return ErrNotAuthor
+	}
+	text = strings.TrimSpace(text)
+	if text == "" {
+		return ErrEmptyCommentText
+	}
+	c.Text = text
+	c.UpdatedAt = time.Now().UTC()
+	return nil
+}
