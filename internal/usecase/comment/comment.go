e0d642f6c182d7735df5c04bf739779e915c79cb
Stepan Semenov <ishee11@gmail.com>
2025-12-12 08:17:22 +1000

    add context to usecase methods

--- internal/usecase/comment/comment.go
+++ internal/usecase/comment/comment.go
@@ -11,6 +11,7 @@
 package comment
 
 import (
+	"context"
 	"fmt"
 	"github.com/evrone/go-clean-template/internal/entity"
 	"github.com/evrone/go-clean-template/internal/repo"
@@ -32,12 +33,12 @@ func New(repo repo.CommentRepository) *UseCase {
 // Проверяет текст комментария через сущность Comment.
 // Сохраняет комментарий через репозиторий.
 // Возвращает созданный комментарий или ошибку.
-func (uc *UseCase) CreateComment(userID, entityID int64, text string) (*entity.Comment, error) {
+func (uc *UseCase) CreateComment(ctx context.Context, userID, entityID int64, text string) (*entity.Comment, error) {
 	c, err := entity.NewComment(entityID, userID, text)
 	if err != nil {
 		return nil, fmt.Errorf("CommentUseCase - CreateComment - entity.NewComment: %w", err)
 	}
-	if err = uc.repo.CreateComment(c); err != nil {
+	if err = uc.repo.CreateComment(ctx, c); err != nil {
 		return nil, fmt.Errorf("CommentUseCase - CreateComment - uc.repo.CreateComment: %w", err)
 	}
 	return c, nil
@@ -47,29 +48,29 @@ func (uc *UseCase) CreateComment(userID, entityID int64, text string) (*entity.C
 // Проверяет, что пользователь является автором комментария.
 // Вызывает метод UpdateCommentText сущности Comment и сохраняет изменения в репозитории.
 // Возвращает ошибку, если комментарий не найден или пользователь не автор.
-func (uc *UseCase) UpdateComment(id, userID int64, text string) error {
-	c, err := uc.repo.GetCommentByID(id)
+func (uc *UseCase) UpdateComment(ctx context.Context, id, userID int64, text string) error {
+	c, err := uc.repo.GetCommentByID(ctx, id)
 	if err != nil {
 		return fmt.Errorf("CommentUseCase - UpdateComment - uc.repo.GetCommentByID: %w", err)
 	}
 	if err = c.UpdateComment(userID, text); err != nil {
 		return fmt.Errorf("CommentUseCase - UpdateCommentText - c.UpdateCommentText: %w", err)
 	}
-	return uc.repo.UpdateComment(c)
+	return uc.repo.UpdateComment(ctx, c)
 }
 
 // ListComments возвращает список комментариев для указанной сущности.
 // Поддерживает пагинацию через page и limit, а также сортировку по дате через sortAsc.
 // Обращается к репозиторию для получения данных.
 // Возвращает срез комментариев или ошибку.
-func (uc *UseCase) ListComments(entityID, page, limit int64, sortAsc bool) ([]*entity.Comment, error) {
-	comments, err := uc.repo.ListCommentByEntity(entityID, page, limit, sortAsc)
+func (uc *UseCase) ListComments(ctx context.Context, entityID, page, limit int64, sortAsc bool) ([]*entity.Comment, error) {
+	comments, err := uc.repo.ListCommentByEntity(ctx, entityID, page, limit, sortAsc)
 	if err != nil {
 		return nil, fmt.Errorf("CommentUseCase - ListComments - uc.repo.ListCommentByEntity: %w", err)
 	}
 	return comments, nil
 }
 
-func (uc *UseCase) GetCommentByID(id int64) (*entity.Comment, error) {
-	return uc.repo.GetCommentByID(id)
+func (uc *UseCase) GetCommentByID(ctx context.Context, id int64) (*entity.Comment, error) {
+	return uc.repo.GetCommentByID(ctx, id)
 }
