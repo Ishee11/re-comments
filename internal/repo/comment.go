e5926bd8efc9456c23c4aa18374ce1773ad994e5
Stepan Semenov <ishee11@gmail.com>
2025-12-12 08:15:30 +1000

    Add go:generate mockgen

--- internal/repo/comment.go
+++ internal/repo/comment.go
@@ -7,6 +7,7 @@
 package repo
 
 import (
+	"context"
 	"github.com/evrone/go-clean-template/internal/entity"
 )
 
@@ -16,8 +17,8 @@ import (
 //
 //go:generate mockgen -source=../repo/comment.go -destination=../usecase/mock/mock_comment_repo.go -package=mock
 type CommentRepository interface {
-	CreateComment(c *entity.Comment) error
-	UpdateComment(c *entity.Comment) error
-	ListCommentByEntity(entityID int64, page int64, limit int64, sortAsc bool) ([]*entity.Comment, error)
-	GetCommentByID(id int64) (*entity.Comment, error)
+	CreateComment(ctx context.Context, c *entity.Comment) error
+	UpdateComment(ctx context.Context, c *entity.Comment) error
+	ListCommentByEntity(ctx context.Context, entityID int64, page int64, limit int64, sortAsc bool) ([]*entity.Comment, error)
+	GetCommentByID(ctx context.Context, id int64) (*entity.Comment, error)
 }
